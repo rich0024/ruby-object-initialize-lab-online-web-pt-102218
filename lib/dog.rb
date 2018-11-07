2f012858a4054bb8a6d69c1cbd33dc975fee397c
Richard Yance <yance0022@gmail.com>
2018-11-07 21:54:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+class Dog
+
+  def initialize(name, breed)
+    @breed = breed
+    @name = name
+  end
+
+
+  def initialize(name, breed = "Mutt")
+    @breed = breed
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
